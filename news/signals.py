ae3fd79c867eb9177d7f1349562b449d6e32c462
1Mikl <zel_726@mail.ru>
2023-04-10 21:57:47 +0300

    Home Work 7.5 (HW-03) Redis,Celery

--- news/signals.py
+++ news/signals.py
@@ -5,26 +5,27 @@ from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from .models import PostCategory
+from .tasks import send_notifications
 
 
-def send_notifications(preview, pk, title, subscribers):
-    html_content = render_to_string(
-        'post_created_email.html',
-        {
-            'text': preview,
-            'Link': f'{settings.SITE_URL}/news/{pk}',
-        }
-    )
-
-    msg = EmailMultiAlternatives(
-        subject=title,
-        body='',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=subscribers,
-    )
-
-    msg.attach_alternative(html_content, 'text/html')
-    msg.send()
+# def send_notifications(preview, pk, title, subscribers):
+#     html_content = render_to_string(
+#         'post_created_email.html',
+#         {
+#             'text': preview,
+#             'Link': f'{settings.SITE_URL}/news/{pk}',
+#         }
+#     )
+#
+#     msg = EmailMultiAlternatives(
+#         subject=title,
+#         body='',
+#         from_email=settings.DEFAULT_FROM_EMAIL,
+#         to=subscribers,
+#     )
+#
+#     msg.attach_alternative(html_content, 'text/html')
+#     msg.send()
 
 @receiver(m2m_changed,  sender=PostCategory)
 def notify_about_new_post(sender, instance, **kwargs):
